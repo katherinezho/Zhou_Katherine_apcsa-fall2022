a111b08bed6481ad5a6a807bce40171d9e820aa1
katherinezho <76193489+katherinezho@users.noreply.github.com>
2022-08-18 11:06:06 -0700

    Unit 0 Complete

---
+++ Unit-00/src/AsciiArt.java
@@ -0,0 +1,37 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class AsciiArt
+{
+	public static void main ( String[] args )
+	{
+		System.out.println("Katherine Zhou \n\n" );
+		System.out.println("Penguin" );
+		System.out.println("\n\n\n\n" );
+
+		System.out.println("               ________              " );
+		System.out.println("              /        \\             " );
+		System.out.println("             /  o   O   \\            " );
+		System.out.println("            /            \\           " );
+		System.out.println("           |	  V      |           " );
+		System.out.println("           |             |            " );
+		System.out.println("          /|             |\\          " );
+		System.out.println("         / |             | \\        " );
+		System.out.println("        /  |             |  \\        " );
+		System.out.println("       /___|             |___\\       " );
+		System.out.println("           \\____________/            " );
+		System.out.println("               W     W    	       " );
+
+		//add other output
+
+		System.out.println(" \n\n\n\nHelpFul Hints" );
+		System.out.println("\\\\ draws one backslash on the screen!\n" );
+		System.out.println("\\\" draws one double quote on the screen!\n" );
+		System.out.println("\\\' draws one single quote on the screen!\n" ); 
+	}
+}
---
+++ Unit-00/src/AsciiBox.java
@@ -0,0 +1,32 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class AsciiBox
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Katherine Zhou \t  8/17/22 \n\n" );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("KKKKKKKKKKKKKKKKKKKKKKKKK " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("TTTTTTTTTTTTTTTTTTTTTTTTT " );
+		System.out.println("ZZZZZZZZZZZZZZZZZZZZZZZZZ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+
+	}
+}
+
