30a96c0bed0b323d7df04b478b0bfefa9af7cecc
katherinezho <76193489+katherinezho@users.noreply.github.com>
2022-08-18 20:34:56 -0700

    Update Variables.java

--- Unit-00/src/Variables.java
+++ Unit-00/src/Variables.java
@@ -15,18 +15,27 @@ public class Variables
 		//byte		short		int 		long
 		//float		double
 		//char      boolean		String
+		
 
 		//integer variables
-		byte byteOne = 127;
+		byte byteVariable = 127;
+		short shortVariable = 32767;
+		int intVariable = 5;
+		long longVariable = 6;
 
 
 		//decimal variables
+		float floatVariable = 3.1415926;
+		double doubleVariable = 6.28;
 
 
 		//other integer types
+		char charVariable = 65;
 
 
 		//other types
+		boolean booleanVariable = true;
+		String stringVariable = "heyyyyy";
 
 
 		//output your information here
@@ -34,7 +43,26 @@ public class Variables
 		System.out.println("*                               *");
 		System.out.println("*        integer types          *");
 		System.out.println("*                               *");
-		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+		System.out.println("*8 bit - byteVariable = "+byteVariable+"\t\t*");
+		System.out.println("*16 bit - shortVariable = "+shortVariable+"\t\t*");
+		System.out.println("*32 bit - intVariable = "+intVariable+"\t\t*");
+		System.out.println("*64 bit - longVariable = "+longVariable+"\t\t*");	
+		System.out.println("*                               *");
+		System.out.println("*        decimal types          *");
+		System.out.println("*32 bit - floatVariable = "+floatVariable+"\t\t*");
+		System.out.println("*64 bit - doubleVariable = "+doubleVariable+"\t\t*");
+		System.out.println("*                               *");
+		System.out.println("*        other integer types          *");
+		System.out.println("*16 bit - charVariable = "+charVariable+"\t\t*");
+		System.out.println("*                               *");
+		System.out.println("*        other types          *");
+		System.out.println("*booleanVariable = "+booleanVariable+"\t\t*");
+		System.out.println("*stringVariable = "+stringVariable+"\t\t*");
+		System.out.println("/////////////////////////////////");
+		
+		
+		
+
 
 
 
