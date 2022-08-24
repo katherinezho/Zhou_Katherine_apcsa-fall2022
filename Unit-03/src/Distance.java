323a9673842a4070208e27dc02f5865233071360
katherinezho <76193489+katherinezho@users.noreply.github.com>
2022-08-23 20:13:29 -0700

    Update Distance.java

--- Unit-03/src/Distance.java
+++ Unit-03/src/Distance.java
@@ -17,23 +17,36 @@ public class Distance
 
 	public Distance()
 	{
+		xOne = 0;
+		yOne = 0;
+		xTwo = 0;
+		yTwo = 0;
 	}
 
 	public Distance(int x1, int y1, int x2, int y2)
 	{
+		xOne = x1;
+		yOne = y1;
+		xTwo = x2;
+		yTwo = y2;
 	}
 
 	public void setCoordinates(int x1, int y1, int x2, int y2)
 	{
+		xOne = x1;
+		yOne = y1;
+		xTwo = x2;
+		yTwo = y2;
 	}
 
 	public void calcDistance()
 	{
+		distance = sqrt((x2-x1)(x2-x1) + (y2-y1)(y2-y1));
 	}
 	
 	public double getDistance()
 	{
-		return 0.0;
+		return distance;
 	}
 	
 	public void print()
