2717f6f2d9b2bcfce091fe204943aba675d9bc0d
katherinezho <76193489+katherinezho@users.noreply.github.com>
2022-08-23 20:19:28 -0700

    Update DistanceRunner.java

--- Unit-03/src/DistanceRunner.java
+++ Unit-03/src/DistanceRunner.java
@@ -14,7 +14,26 @@ public class DistanceRunner
 {
 	public static void main( String[] args )
 	{
-		//add test cases	
-			
+    //variables and instantiate
+    int x1, y1, x2, y2;
+    Scanner keyboard = new Scanner(System.in);
+    //get values
+    System.out.println("Enter X1 :: ");
+    x1 = keyboard.nextInt();
+    System.out.println("Enter Y1 :: ");
+    y1 = keyboard.nextInt();
+    System.out.println("Enter X2 :: ");
+    x2 = keyboard.nextInt();
+    System.out.println("Enter Y2 :: ");
+    y2 = keyboard.nextInt();
+
+    //Instantiate
+    Distance distance = new Distance();
+    //call methods
+    distance.setCoordinates(x1, y1, x2, y2);
+    distance.calcDistance();
+    System.out.println("distance == " + distance.getDistance());
+    
+
 	}
 }
