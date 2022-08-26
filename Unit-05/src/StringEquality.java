bcc1c08b5e7b8235ec5f4fa367bde9d3546a4535
katherinezho <76193489+katherinezho@users.noreply.github.com>
2022-08-26 11:40:31 -0700

    Updates

--- Unit-05/src/StringEquality.java
+++ Unit-05/src/StringEquality.java
@@ -18,19 +18,28 @@ public class StringEquality
 
 	public StringEquality(String one, String two)
 	{
+		setWords(one, two);
 	}
 
 	public void setWords(String one, String two)
 	{
+		wordOne = one;
+		wordTwo = two;
 	}
 
 	public boolean checkEquality( )
 	{
+		if(wordOne.equals(wordTwo)){
+			return true;
+		}
 		return false;
 	}
 
 	public String toString()
 	{
-		return wordOne + " does not have the same letters as " + wordTwo + "\n";
+		if (checkEquality()) {
+			return wordOne + " has the same letters as " + wordTwo;
+		}
+		return wordOne + " does not have the same letters as " + wordTwo;
 	}
 }
--- Unit-05/src/StringEqualityRunner.java
+++ Unit-05/src/StringEqualityRunner.java
@@ -5,14 +5,28 @@
 //Date -
 //Class -
 //Lab  - 
-
+import java.util.Scanner;
 import static java.lang.System.*;
 
 public class StringEqualityRunner
 {
 	public static void main( String args[] )
 	{
-		//add test cases
+		//Instantiate Scanner Object
+		Scanner keyboard = new Scanner(System.in);
+		
+		//Collect String One
+		System.out.println("Enter String One :: ");
+		String one = keyboard.next();
+		
+		//Collect String Two
+		System.out.println("Enter String Two :: ");
+		String two = keyboard.next();
+		
+		
+		StringEquality stringCheck = new StringEquality( one,  two);
+		
+		System.out.println(stringCheck);
 		
 	}
 }
