15deb52f880916d754eee5e9dd9ffd5209d0bd28
katherinezho <76193489+katherinezho@users.noreply.github.com>
2022-08-26 10:42:33 -0700

    updates

--- Unit-04/src/NumberVerifyRunner.java
+++ Unit-04/src/NumberVerifyRunner.java
@@ -14,9 +14,16 @@ public class NumberVerifyRunner
 	public static void main ( String[] args )
 	{
 		//add in input
-		System.out.println("5 is odd :: " + NumberVerify.isOdd(5));
-		System.out.println("5 is even :: " + NumberVerify.isEven(5));
+		System.out.println("6 is odd :: " + NumberVerify.isOdd(6));
+		System.out.println("6 is even :: " + NumberVerify.isEven(6));
 		
 		//add in more test cases
+		System.out.println("9 is odd :: " + NumberVerify.isOdd(9));
+		System.out.println("9 is even :: " + NumberVerify.isEven(9));
+		
+		System.out.println("10223 is odd :: " + NumberVerify.isOdd(10223));
+		System.out.println("10223 is even :: " + NumberVerify.isEven(10223));
+		
+		
 	}
 }
