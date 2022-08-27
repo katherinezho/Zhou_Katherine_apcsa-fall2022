742f882a91f2a1d442f47dbc1a9b557f1295e2f6
katherinezho <76193489+katherinezho@users.noreply.github.com>
2022-08-27 00:10:53 -0700

    Update WordsCompareRunner.java

--- Unit-05/src/WordsCompareRunner.java
+++ Unit-05/src/WordsCompareRunner.java
@@ -7,12 +7,27 @@
 //Lab  -
 
 import static java.lang.System.*;
+import java.util.Scanner;
 
 public class WordsCompareRunner
 {
    public static void main( String args[] )
    {
 		//add test cases
+	   Scanner keyboard = new Scanner(System.in);
+	   System.out.print.ln("Enter Word One :: ");
+	   
+	   String one = keyboard.next();
+	   
+	   System.out.print.ln("Enter Word Two :: ");
+	   
+	   String two = keyboard.next();
+	   
+	   WordsCompare compare = new WordsCompare(one, two);
+	   
+	   System.out.println(compare);
+	   
+	   
 
 	}
 }
