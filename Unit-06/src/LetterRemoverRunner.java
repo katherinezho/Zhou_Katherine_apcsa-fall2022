aa447fe630c35502e5af86d9fda7ca63b627f9a6
katherinezho <76193489+katherinezho@users.noreply.github.com>
2022-09-04 01:36:04 -0700

    Update LetterRemoverRunner.java

--- Unit-06/src/LetterRemoverRunner.java
+++ Unit-06/src/LetterRemoverRunner.java
@@ -3,12 +3,21 @@
 //Name -
 
 import static java.lang.System.*;
+import java.util.Scanner;
 
 public class LetterRemoverRunner
 {
 	public static void main( String args[] )
 	{
-		//add test cases		
-											
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("Enter String to Edit :: ");
+		String s = keyboard.next();
+		
+		System.out.println("Enter Letter to Remove :: ");
+		char rem = keyboard.next().charAt(0);
+		
+		LetterRemover letterRemove = new LetterRemover(s, rem);
+		System.out.println(letterRemove);
 	}
 }
