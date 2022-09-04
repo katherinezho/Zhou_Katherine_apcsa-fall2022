ee75328a72db1c247813b9140a7ed99cf44018e8
katherinezho <76193489+katherinezho@users.noreply.github.com>
2022-09-04 02:16:14 -0700

    Update Perfect.java

--- Unit-06/src/Perfect.java
+++ Unit-06/src/Perfect.java
@@ -9,14 +9,34 @@ public class Perfect
    private int number;
 
 	//add constructors
+	public Perfect(){
+	}
+	
+	public Perfect(int num){
+		setPerfect(num);
+	}
 
 	//add a set method
+	public void setPerfect(num){
+		number = num;
+	}
 
 	public boolean isPerfect()
 	{
-		return false;
+		int total = 0;
+		for(int i = 1; i < number; i++){
+			if(number%i == 0)
+				total = total + i;
+		}
+		if(total == number)
+			return true;
+		else
+			return false;
 	}
 
-	//add a toString	
+	//add a toString
+	public String toString(){
+		return "" + isPerfect();
+	}
 	
 }
