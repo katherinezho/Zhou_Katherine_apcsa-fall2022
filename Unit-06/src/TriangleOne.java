d57eab126872ed52aeaa70926672ff531b76d838
katherinezho <76193489+katherinezho@users.noreply.github.com>
2022-09-04 15:33:27 -0700

    Update TriangleOne.java

--- Unit-06/src/TriangleOne.java
+++ Unit-06/src/TriangleOne.java
@@ -15,13 +15,19 @@ public class TriangleOne
 
 	public TriangleOne(String s)
 	{
+		setWord(s);
 	}
 
 	public void setWord(String s)
 	{
+		word = s;
+
 	}
 
 	public void print( )
 	{
+		for(int i = word.length(); i > 0; i--){
+			System.out.println(word.substring(0,i));
+		}
 	}
 }
