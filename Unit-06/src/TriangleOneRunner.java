6ffd87b909a729bbb5e51ccf11dafc9aa4ced064
katherinezho <76193489+katherinezho@users.noreply.github.com>
2022-09-04 15:33:33 -0700

    Update TriangleOneRunner.java

--- Unit-06/src/TriangleOneRunner.java
+++ Unit-06/src/TriangleOneRunner.java
@@ -3,11 +3,19 @@
 //Name
 
 import static java.lang.System.*;
+import java.util.Scanner;
 
 public class TriangleOneRunner
 {
 	public static void main ( String[] args )
 	{
-		//add test cases		
+		//add test cases	
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("Enter Word :: ");
+		String s = keyboard.next();
+		
+		TriangleOne wordTriangle = new TriangleOne(s);
+		wordTriangle.print();
 	}
 }
