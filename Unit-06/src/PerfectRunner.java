7cc9e28a5dbd16d715529add75c0fa8eda1c2756
katherinezho <76193489+katherinezho@users.noreply.github.com>
2022-09-04 02:16:17 -0700

    Update PerfectRunner.java

--- Unit-06/src/PerfectRunner.java
+++ Unit-06/src/PerfectRunner.java
@@ -3,12 +3,24 @@
 //Name -
 
 import static java.lang.System.*; 
+import java.util.Scanner;
 
 public class PerfectRunner
 {
 	public static void main( String args[] )
 	{
 		//add test cases
+		
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("Enter Number :: ");
+		int num = keyboard.nextInt();
+		
+		
+		Perfect perfectNumber = new Perfect(num);
+		System.out.println(perfectNumber);
+
+		
 																
 	}
 }
