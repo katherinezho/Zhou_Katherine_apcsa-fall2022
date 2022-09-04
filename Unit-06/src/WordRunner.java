ca713ac7aa7bd3f24e38ddd890b55774c231d44c
katherinezho <76193489+katherinezho@users.noreply.github.com>
2022-09-04 16:05:16 -0700

    Update WordRunner.java

--- Unit-06/src/WordRunner.java
+++ Unit-06/src/WordRunner.java
@@ -3,11 +3,19 @@
 //Name
 
 import static java.lang.System.*;
+import java.util.Scanner;
 
 public class WordRunner
 {
 	public static void main ( String[] args )
 	{
-		//add test cases		
+		//add test cases
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Enter Word :: ");
+		
+		String s = keyboard.next();
+		
+		Word wordPrint = new Word(s);
+		System.out.println(wordPrint);
 	}
 }
