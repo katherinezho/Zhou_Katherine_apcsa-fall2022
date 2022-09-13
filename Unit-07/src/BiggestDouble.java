58a3eb60d9ca0f87f67640c2e9186897f95eff5d
katherinezho <76193489+katherinezho@users.noreply.github.com>
2022-09-12 23:07:54 -0700

    Update BiggestDouble.java

--- Unit-07/src/BiggestDouble.java
+++ Unit-07/src/BiggestDouble.java
@@ -10,24 +10,39 @@ public class BiggestDouble
 
 	public BiggestDouble()
 	{
-		this(0,0,0,0);
+		setDoubles(0,0,0,0);
 	}
 
 	public BiggestDouble(double a, double b, double c, double d)
 	{
+		setDoubles(a, b, c, d);
 	}
 
 	public void setDoubles(double a, double b, double c, double d)
 	{
+		one = a;
+		two = b;
+		three = c;
+		four = d;
 	}
 
 	public double getBiggest()
 	{
+		if(one > two && one > three && one > four)
+			return one;
+		if(two > one && two > three && two > four)
+			return two;
+		if(three > two && three > one && three > four)
+			return three;
+		if(four > two && four > three && four > one)
+			return four;
 		return 0.0;
+		
+		
 	}
 
 	public String toString()
 	{
-	   return "";
+	   return "" + one + " " + two + " " + three + " " + four;
 	}
 }
