17ada1d43c575a11c551f0f848aa018f4d624630
katherinezho <76193489+katherinezho@users.noreply.github.com>
2022-09-12 23:02:02 -0700

    Update TriangleFive.java

--- Unit-07/src/TriangleFive.java
+++ Unit-07/src/TriangleFive.java
@@ -15,19 +15,45 @@ public class TriangleFive
 
 	public TriangleFive(char c, int amt)
 	{
+		setLetter(c);
+		setAmount(amt);
 	}
 
 	public void setLetter(char c)
 	{
+		letter = c;
 	}
 
 	public void setAmount(int amt)
 	{
+		amount = amt;
+	}
+	public String triangleString(){
+		String triangle = "";
+		int count = amount;
+		char letterCount = letter;
+		for(int i = amount; i > 0; i++){
+			for(int j = i; i > 0; j++){
+				for(int k = count; k > 0; k++){
+					triangle += letterCount;
+				}
+				if(letterCount != 90)
+					letterCount++;
+				else
+					letterCount = 45;
+				count--;
+			}
+			count = amount;
+			letterCount = letter;
+			triangle += "\n";
+			
+		}
+		return triangle;
 	}
 
 	public String toString()
 	{
-		String output="";
-		return output;
+		
+		return triangleString();
 	}
 }
