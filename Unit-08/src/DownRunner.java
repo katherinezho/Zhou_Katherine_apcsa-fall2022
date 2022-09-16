15f669d564a7761940daa3dee7934b2c1b9e7aef
katherinezho <76193489+katherinezho@users.noreply.github.com>
2022-09-15 20:05:40 -0700

    Update DownRunner.java

--- Unit-08/src/DownRunner.java
+++ Unit-08/src/DownRunner.java
@@ -8,7 +8,7 @@ public class DownRunner
 	public static void main( String args[] )
 	{	
 		RayDown s = new RayDown();
-		s.go(int[] {5, 4, 3, 2, 1});
-		s.go(int[] {5, 4, 3, 2, 3});
+		s.go(new int[] {5, 4, 3, 2, 1});
+		s.go(new int[] {5, 4, 3, 2, 3});
 	}
 }
