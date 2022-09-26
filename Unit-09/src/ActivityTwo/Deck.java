feb80d846c0b047b9b4e2535f9373dc43af083ba
katherinezho <76193489+katherinezho@users.noreply.github.com>
2022-09-26 15:34:27 -0700

    Update Deck.java

--- Unit-09/src/ActivityTwo/Deck.java
+++ Unit-09/src/ActivityTwo/Deck.java
@@ -11,7 +11,7 @@ public class Deck {
 	/**
 	 * cards contains all the cards in the deck.
 	 */
-	private List<Card> cards;
+	private Card[] cards;
 
 	/**
 	 * size is the number of not-yet-dealt cards.
@@ -31,6 +31,16 @@ public class Deck {
 	 */
 	public Deck(String[] ranks, String[] suits, int[] values) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		//final array length
+		size = ranks.length * suits.length;
+		cards = new Card[size];
+		for(int x = 0; x < size; x++){
+			for(int i = 0; i < suits.length; i ++){
+				for(int j = 0; j < ranks.length; j++){
+					cards[x] = new Card(ranks[j], suits[i], values[j]);
+				}
+			}
+		}
 	}
 
 
@@ -40,6 +50,10 @@ public class Deck {
 	 */
 	public boolean isEmpty() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		if(size == 0)
+			return true;
+		else
+			return false;
 	}
 
 	/**
@@ -48,6 +62,7 @@ public class Deck {
 	 */
 	public int size() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		return size;
 	}
 
 	/**
@@ -65,6 +80,10 @@ public class Deck {
 	 */
 	public Card deal() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		if(isEmpty())
+			return null;
+		size--;
+		Card deal = cards[size];
 	}
 
 	/**
