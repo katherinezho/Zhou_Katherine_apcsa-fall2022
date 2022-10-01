13a91eb65fae6995eb54e08c4e6eefae11d98c7a
katherinezho <76193489+katherinezho@users.noreply.github.com>
2022-09-30 22:30:36 -0700

    Update ListDown.java

--- Unit9Students-ArrayLists/ListDown.java
+++ Unit9Students-ArrayLists/ListDown.java
@@ -12,6 +12,11 @@ public class ListDown
 	//are in decreasing order [31,12,6,2,1]
 	public static boolean go(List<Integer> numArray)
 	{
+		for(int i = 1; i < numArray.size(); i++) {
+			if (numArray.get(i-1)<=numArray.get(i)) {
+				return false;
+			}
+		}
 		return true;
 	}	
 }
