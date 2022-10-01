d32762001741e19306a9bee0a662cd60def4dfc2
katherinezho <76193489+katherinezho@users.noreply.github.com>
2022-09-30 22:45:20 -0700

    Update ListOddToEven.java

--- Unit9Students-ArrayLists/ListOddToEven.java
+++ Unit9Students-ArrayLists/ListOddToEven.java
@@ -10,6 +10,25 @@ public class ListOddToEven
 {
 	public static boolean go( List<Integer> ray )
 	{
-		return false;
+		int countEven = 0;
+		int countOdd = 0;
+		boolean foundEven = false;
+		boolean foundOdd = false;
+		for (int i = 0; i < ray.size() && !(foundOdd && foundEven); i++) {
+			if(!foundOdd && ray.get(i) %  2 == 1) {
+				countOdd = (i);
+				foundOdd = true;
+			}
+			if(!foundE && ray.get(ray.size()-1-i) %  2 == 0) {
+				countEven = ray.size()-i;
+				foundEven = true;
+			}
+		}
+		if (foundOdd) {
+			return (countO - countE);
+		}
+		else {
+			return -1;
+		}
 	}
 }
