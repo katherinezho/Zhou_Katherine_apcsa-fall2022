5e44c2bdc05ce841f24244ac8ea61ae5049d2cdc
katherinezho <76193489+katherinezho@users.noreply.github.com>
2022-10-10 23:52:59 -0700

    Update ToyStore.java

--- Unit-11/src/ToyStore.java
+++ Unit-11/src/ToyStore.java
@@ -14,28 +14,70 @@ public class ToyStore
 
 	public ToyStore()
 	{
+		this("");
+	}
+	
+	public ToyStore(String toys)
+	{
+		toyList = new ArrayList<Toy>();
+		loadToys(toys);
 	}
 
 	public void loadToys( String toys )
 	{
+		if (toys == "")
+			return;
+		for (String name : toys.split(" ")) {
+			Toy t = getThatToy(name);
+			if (t == null)
+				toyList.add(new Toy(name));
+			else
+				t.setCount(t.getCount()+1);
+		}
+			
+	
 	}
   
   	public Toy getThatToy( String nm )
   	{
+  		for (Toy x: toyList) {
+  			if(x.getName().equals(nm))
+  				return x;
+  		}
   		return null;
   	}
   
   	public String getMostFrequentToy()
   	{
-  		return "";
+  		Toy max = new Toy();
+  		max.setCount(0);
+  		for (Toy t: toyList) {
+  			if (t.getCount() > max.getCount())
+  				max = t;
+  		}
+  		return max.getName();
   	}  
   
   	public void sortToysByCount()
   	{
+  		boolean sorted = false;
+  		while (!sorted) {
+  			sorted = true;
+	  		for(int i = 0; i < toyList.size()-1; i++) {
+	  			Toy t = toyList.get(i);
+	  			Toy tp = toyList.get(i+1);
+	  			if(t.getCount() < tp.getCount()) {
+	  				sorted = false;
+	  				Toy temp = t;
+	  				toyList.set(i, tp);
+	  				toyList.set(i + 1, temp);
+	  			}
+	  		}
+  		}
   	}  
   	  
 	public String toString()
 	{
-	   return "";
+	   return toyList.toString();
 	}
 }
