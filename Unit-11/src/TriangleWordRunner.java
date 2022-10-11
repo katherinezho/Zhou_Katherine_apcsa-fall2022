72ce842429a7d6daacb11fe0e4a45f17aea54bf6
katherinezho <76193489+katherinezho@users.noreply.github.com>
2022-10-10 23:56:14 -0700

    Update TriangleWordRunner.java

--- Unit-11/src/TriangleWordRunner.java
+++ Unit-11/src/TriangleWordRunner.java
@@ -9,6 +9,18 @@ public class TriangleWordRunner
 {
 	public static void main( String args[] )
 	{
+		Scanner keyboard = new Scanner(System.in);
+	   	String yes="";
+			do{
+				out.print("\nEnter a word :: ");
+				String word = keyboard.next();	
+				
+				//call the printWord method
+	 			TriangleWord.printTriangle(word);
+
+				System.out.print("\nDo you want to enter more sample input? ");
+				yes=keyboard.next().charAt(0).toUpperCase();			
+			}while(yes.equals("Y"));
 		
 	}
 }
