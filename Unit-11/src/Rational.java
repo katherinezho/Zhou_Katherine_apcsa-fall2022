f383684aa9554c58c556a79e186b9f72d25b035e
katherinezho <76193489+katherinezho@users.noreply.github.com>
2022-10-10 23:41:22 -0700

    Update Rational.java

--- Unit-11/src/Rational.java
+++ Unit-11/src/Rational.java
@@ -1,3 +1,5 @@
+
+
 //(c) A+ Computer Science
 //www.apluscompsci.com
 //Name -
@@ -7,115 +9,105 @@ import static java.lang.System.*;
 class Rational implements Comparable<Rational>
 {
 	//add two instance variables
-	private int numberOne;
-	private int numberTwo;
-	private int newNum;
-	private int newDen;
-
+	private int num;
+	private int den;
 	//write two constructors
 	public Rational() {
-		
+		this(1, 1);
 	}
-	public Rational(int one, int two) {
-		setRational(one, two);
+	public Rational(int n,int d) {
+		setRational(n, d);
 	}
-
 
 	//write a setRational method
-	public void setRational(int one, int two) {
-		numberOne = one;
-		numberTwo = two;
+	public void setRational(int n, int d) {
+		setNum(n);
+		setDen(d);
+		reduce();
 	}
-	
 
 	//write  a set method for numerator and denominator
-
+	public void setNum(int n) {
+		num = n;
+	}
+	public void setDen(int d) {
+		den = d;
+	}
 	
-	public void add(Rational other)
+	public void add(Rational  other)
 	{
 		//num1/den1 + num2/den2 
 		//new numerator = (num1 * den2 + num2 * den1)
 		//new denominator = (den1 * den2)
-		int numeratorOne = Integer.toString(numberOne).charAt(0);
-		int numeratorTwo = Integer.toString(numberTwo).charAt(0);
-		
-		int denominatorOne = Integer.toString(numberOne).charAt(2);
-		int denominatorTwo = Integer.toString(numberTwo).charAt(2);
-		
-		newNum = (numeratorOne * denominatorTwo + numeratorTwo * denominatorOne);
-		newDen = (denominatorOne * denominatorTwo);
-		
+		setNum(num * other.getDen() + other.getNum() * den);
+		setDen(den * other.getDen());
 
 
 		reduce();
 	}
 
 	private void reduce()
-	
-	
 	{
-		int divisor = gcd(newNum, newDen);
-		newNum = newNum/divisor;
-		newDen = newDen/divisor;
-
+		int gcd = gcd(Math.abs(num), Math.abs(den));
+		setNum(num/gcd);
+		setDen(den/gcd);
 
 
 	}
 
 	private int gcd(int numOne, int numTwo)
 	{
-		int max;
-		int gcd = 1;
-		if(newNum>newDen)
-			max = newDen;
-		else
-			max = newNum;
-			
-		for(int i = 0; i <max; i++) {
-			if(newNum%i == 0 && newDen%i ==0) {
-				gcd = i;
-			}
+		for(int i = numOne > numTwo ? numOne: numTwo; i>0; i--) {
+			if(numOne % i == 0 && numTwo % i == 0)
+				return i;
 		}
-
-
-		return gcd;
+		return 1;
 	}
 
 	public Object clone ()
 	{
-		return "";
+		return new Rational(getNum(), getDen());
 	}
 
 
 	//ACCESSORS
 
 	//write get methods for numerator and denominator
-	public int getNumerator() {
-		return newNum;
-	}
-	public int getDenominator() {
-		return newDen;
+	public int getNum() {
+		return num;
 	}
 	
+	public int getDen() {
+		return den;
+	}
 	
 	public boolean equals( Object obj)
 	{
-
-
+		if(num == ((Rational) obj).getNum() && den == ((Rational) obj).getDen()) 
+			return true;
 		return false;
 	}
 
 	public int compareTo(Rational other)
 	{
-
-
-		return -1;
+		if (equals(other))
+			return 0;
+		Rational temp = new Rational(1, 1);
+		temp.add(new Rational (-1 * (num+den), den));
+		temp.add(other);
+		if((temp.getNum() > 0 && temp.getDen() > 0 ) || (temp.getNum() < 0 && temp.getDen() < 0 )) {
+			return -1;
+		}
+		return 1;
 	}
 
 
 
 	
 	//write  toString() method
+	public String toString() {
+		return num + "/" + den;
+	}
 	
 	
 }
