473d98f6182b5c43ee17c0bbb84061c0ae169e27
katherinezho <76193489+katherinezho@users.noreply.github.com>
2022-10-10 23:53:36 -0700

    Update ToyStoreRunner.java

--- Unit-11/src/ToyStoreRunner.java
+++ Unit-11/src/ToyStoreRunner.java
@@ -11,6 +11,12 @@ public class ToyStoreRunner
 {
 	public static void main( String args[] )
 	{
-
+		ToyStore test = new ToyStore("");
+		System.out.println(test);
+		test.loadToys("sorry bat sorry sorry sorry train train teddy teddy ball ball");
+		System.out.println(test);
+		System.out.println(test.getMostFrequentToy());
+		test.sortToysByCount();
+		System.out.println(test);
 	}
 }
