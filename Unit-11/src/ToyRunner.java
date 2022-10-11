20b7e186706c4c590df82f68a9a70324bcfa4ad1
katherinezho <76193489+katherinezho@users.noreply.github.com>
2022-10-10 23:49:54 -0700

    Update ToyRunner.java

--- Unit-11/src/ToyRunner.java
+++ Unit-11/src/ToyRunner.java
@@ -8,5 +8,11 @@ public class ToyRunner
 {
 	public static void main(String[] args)
 	{
+		Toy test = new Toy("rubber duck");
+		System.out.println(test);
+		test.setName("cars");
+		test.setCount(5);
+		System.out.println(test);
+		
 	}
 }
