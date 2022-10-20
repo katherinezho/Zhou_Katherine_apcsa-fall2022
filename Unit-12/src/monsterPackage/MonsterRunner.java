a1014d38d618dd225753d1a46ca8046fbfe0f2a6
katherinezho <76193489+katherinezho@users.noreply.github.com>
2022-10-19 23:47:50 -0700

    Update MonsterRunner.java

--- Unit-12/src/monsterPackage/MonsterRunner.java
+++ Unit-12/src/monsterPackage/MonsterRunner.java
@@ -13,11 +13,27 @@ public class MonsterRunner
 		Scanner keyboard = new Scanner(System.in);
 		
 		//ask for name and size
+		System.out.println("Enter 1st Monster's Name :: ");
+		String name = keyboard.next();
+		System.out.println("Enter 1st Monster's Size :: ");
+		int size = keyboard.nextInt();
 		
 		//instantiate monster one
+		Skeleton monsterOne = new Skeleton(name, size);
 		
 		//ask for name and size
+		System.out.println("Enter 2nd Monster's Name :: ");
+		System.out.println("Enter 2nd Monster's Size :: ");
+
 		
 		//instantiate monster two
+		Skeleton monsterTwo = new Skeleton(name, size);
+		
+		System.out.println("/nMonster 1 - " + monsterOne.getName() + " " + monsterOne.getSize());
+		System.out.println("/nMonster 1 - " + monsterOne.getName() + " " + monsterOne.getSize());
+		
+		System.out.println(monsterOne);
+
+
 	}
 }
