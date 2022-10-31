3f680857db281aaad8bd5d8cc511a6136bec83ea
katherinezho <76193489+katherinezho@users.noreply.github.com>
2022-10-31 11:05:13 -0700

    Update Recursion234Runner.java

--- Unit-14/src/Recursion234Runner.java
+++ Unit-14/src/Recursion234Runner.java
@@ -11,5 +11,14 @@ public class Recursion234Runner
 		System.out.println(RecursionFunOne.countOddDigits(4532));
 
 		//add more test cases
+		System.out.println(RecursionFunOne.countOddDigits(8922));
+		System.out.println(RecursionFunOne.countOddDigits(30991));
+		System.out.println(RecursionFunOne.countOddDigits(48));
+		System.out.println(RecursionFunOne.countOddDigits(1290));
+		
+		System.out.println(RecursionFunThree.luckyThrees(32934);
+		System.out.println(RecursionFunThree.luckyThrees(9442321);
+		System.out.println(RecursionFunThree.luckyThrees(3333333);
+
 	}
 }
