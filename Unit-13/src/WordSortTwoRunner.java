bb40270ca9e16abc1cc7cd71ba83ad2ae32207e0
katherinezho <76193489+katherinezho@users.noreply.github.com>
2022-11-04 02:37:13 -0700

    Update WordSortTwoRunner.java

--- Unit-13/src/WordSortTwoRunner.java
+++ Unit-13/src/WordSortTwoRunner.java
@@ -18,7 +18,7 @@ public class WordSortTwoRunner
 		for(int i = 0; i<size; i++)
 		{
 			String sentence = file.nextLine();
-			//instantiate a new WordSort
+			wordSortTwo test = new wordSortTwo(sentence);
 		}
 	}
 }
