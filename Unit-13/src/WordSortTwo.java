1232ff6b44ae1f723d3b07194452db658a10555f
katherinezho <76193489+katherinezho@users.noreply.github.com>
2022-11-04 02:37:11 -0700

    Update WordSortTwo.java

--- Unit-13/src/WordSortTwo.java
+++ Unit-13/src/WordSortTwo.java
@@ -12,10 +12,33 @@ public class WordSortTwo
 
 	public WordSortTwo(String sentence)
 	{
+		int length = 1;
+		for(int i = 0; i < sentence.length(); i++) {
+			if(sentence.charAt(i) == ' ') {
+				length++;
+			}
+		}
+		wordRay = new String[length];
+		
+		int i = 0;
+		for(String word : sentence.split(" ")) {
+			wordRay[i] = word;
+			i++;
+		}
+		sort();
 	}
 
 	public void sort()
 	{
+		for(int i = 0; i < wordRay.length-1; i++) {
+			for(int j = i + 1; j < wordRay.length; j++) {
+				if(wordRay[i].compareTo(wordRay[j]) > 0) {
+					String temp = wordRay[i];
+					wordRay[i] = wordRay[j];
+					wordRay[j] = temp;
+				}
+			}
+		}
 	}
 
 	public String toString()
