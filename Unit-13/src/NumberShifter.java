8bca5f110b3961f76691fde4134e36e52f3c4bc8
katherinezho <76193489+katherinezho@users.noreply.github.com>
2022-11-03 23:12:13 -0700

    Update NumberShifter.java

--- Unit-13/src/NumberShifter.java
+++ Unit-13/src/NumberShifter.java
@@ -10,11 +10,28 @@ import static java.lang.System.*;
 
 public class NumberShifter
 {
+	private int[] sevenArray;
 	public static int[] makeLucky7Array( int size)
 	{
-		return null;
+		sevenArray = new int[size];
+		for(int i = 0; i < size; i++) {
+			sevenArray[i] = (int)(Math.random() * 9) + 1;
+		}
+		return sevenArray;
 	}
 	public static void shiftEm(int[] array)
 	{
+		int count = 0;
+		for (int i = 0; i< array.length;i++) {
+			if(array[i] == 7) {
+				array[i] = array[count];
+				array[count++] = 7;
+				count++;
+			}
+		}
+		
+	}
+	public static int[] returnArray(){
+		return sevenArray;
 	}
 }
