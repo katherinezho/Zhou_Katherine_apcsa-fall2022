e7fe61a43d4b923136071023de3b58599963d224
katherinezho <76193489+katherinezho@users.noreply.github.com>
2022-11-09 12:05:56 -0800

    u15

--- Unit-15/src/Block.java
+++ Unit-15/src/Block.java
@@ -21,16 +21,24 @@ public class Block implements Locatable
 	}
 
 	//add other Block constructors - x , y , width, height, color
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	public Block(int x, int y) {
+		this(x, y, 10, 10, Color.black);
+	}
+	 public Block(int x, int y, int width) {
+		 this(x, y, width, 10, Color.black);
+	 }
+	public Block(int x, int y, int width, int height) {
+		this(x, y, width, height, Color.black);
+	   }
+
+	public Block(int x, int y, int width, int height, Color color) {
+		setX(x);
+		setY(y);
+		setWidth(width);
+		setHeight(height);
+		setColor(color);
+	   }
+	   
 	
 	
 	
@@ -39,34 +47,83 @@ public class Block implements Locatable
 
    public void setColor(Color col)
    {
-
-
+	   color = col;
    }
 
    public void draw(Graphics window)
    {
-   	//uncomment after you write the set and get methods
-      //window.setColor(color);
-      //window.fillRect(getX(), getY(), getWidth(), getHeight());
+   
+      window.setColor(color);
+      window.fillRect(getX(), getY(), getWidth(), getHeight());
    }
+  
 
    public void draw(Graphics window, Color col)
    {
+	   window.setColor(col);
+	   window.fillRect(getX(), getY(), getWidth(), getHeight());
 
 
    }
    
-	public boolean equals(Object obj)
-	{
-
-
-
-
+   public boolean equals(Object obj) {
+		if(this.getClass() == obj.getClass() && getX() == ((Block) obj).getX() && getY() == ((Block) obj).getY() && getWidth() == ((Block) obj).getWidth() && getHeight() == ((Block) obj).getHeight() && getColor() == ((Block) obj).getColor())
+				return true;
 		return false;
-	}   
+	}
 
-   //add the other get methods
-    
+	
+	public void setPos(int x, int y) {
+		xPos = x;
+		yPos = y;
+		
+	}
+
+	public void setX(int x) {
+		xPos = x;
+		
+	}
+
+	
+	public void setY(int y) {
+		yPos = y;
+		
+	}
+	public void setWidth(int w) {
+		width = w;
+	}
+	public void setHeight(int h) {
+		height = h;
+	}
+
+	
+	public int getX() {
+		
+		return xPos;
+	}
+
+	
+	public int getY() {
+		return yPos;
+	}   
+	
+	public int getWidth() {
+		return width;
+	}
+	public int getHeight() {
+		return height;
+		
+	}
+	public Color getColor() {
+		return color;
+	}
+	
+
+  
 
    //add a toString() method  - x , y , width, height, color
+	public String toString() {
+		return "" + getX() + " "  + getY() + " " + getWidth() + " " + getHeight() + " " + getColor();
+	
+	}
 }
--- Unit-15/src/Paddle.java
+++ Unit-15/src/Paddle.java
@@ -12,15 +12,37 @@ public class Paddle extends Block
 
    public Paddle()
    {
-		super(10,10);
-      speed =5;
+	   super(10,10);
+	   speed = 5;
    }
 
 
    //add the other Paddle constructors
+   public Paddle(int x, int y) {
+	   super(x,y);
+	   speed = 5;
+   }
+   public Paddle(int x, int y, int width) {
+	   super(x, y, width);
+	   speed = 5;
+   }
+   public Paddle(int x, int y, int width, int height) {
+	   super(x, y, width, height);
+	   speed = 5;
+   }
+   public Paddle(int x, int y, int width, int height, int speed) {
+	   super(x, y, width, height);
+	   this.speed = speed;
+   }
 
-
-
+   public Paddle(int x, int y, int width, int height, Color color) {
+	   super(x, y, width, height, color);
+	   speed = 5;
+   }
+   public Paddle(int x, int y, int width, int height, Color color, int speed) {
+	   super(x, y, width, height, color);
+	   this.speed = speed;
+   }
 
 
 
@@ -41,7 +63,13 @@ public class Paddle extends Block
    }
 
    //add get methods
+   public int getSpeed() {
+	   return speed;
+   }
    
    
    //add a toString() method
+   public String toString() {
+	   return super.toString() + " " + getSpeed();
+   }
 }
